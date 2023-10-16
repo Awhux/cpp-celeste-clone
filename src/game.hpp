ea79d0ffec78de4cb1bd547bd9114d78e58bdfb6
Awhux <brudoismilequatro@gmail.com>
2023-10-16 13:47:08 -0300

    feat: game state

---
+++ src/game.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _GAME_HPP
+#define _GAME_HPP
+
+namespace game {
+  /**
+   * Inline reference to check if the game is running.
+   */
+  inline bool running = true;
+}  // namespace game
+
+#endif
