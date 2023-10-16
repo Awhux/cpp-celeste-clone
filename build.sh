c604ce8ccd9036eb87da46a2fd8ea36d72aab9ab
Awhux <brudoismilequatro@gmail.com>
2023-10-16 13:43:22 -0300

    update: new commands to the build.sh, new commands to work with opengl

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 EXENAME="celeste.exe"
 
+LIBS="-luser32 -lopengl32"
+WARNINGS="-Wno-writable-strings -Wmacro-redefined"
+EXTENSIONS="-std=c++17"
+
 if [ -d "build" ]; then
     rm -rf build
 fi
 
 mkdir build
 
-clang++ -g $(find src -name "*.cpp") -o build/$EXENAME
+clang++ -g $(find src -name "*.cpp") -o build/$EXENAME -Isrc/include -Lsrc/lib $LIBS $WARNINGS $EXTENSIONS
