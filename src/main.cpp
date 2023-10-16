b9165096e4cd5259278cfb59ab1fb98c69d01e63
Awhux <brudoismilequatro@gmail.com>
2023-10-16 13:47:43 -0300

    feat: cpp joining all classes we've created so far. tracing every step to clarify the dev on whats happening

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,26 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+#define _CRT_SECURE_NO_WARNINGS 1
 
+// Local files
+#include "game.hpp"
+#include "window.hpp"
+
+// Folders in src/
+#include "renderer/gl_renderer.hpp"
+#include "utils/logger.hpp"
+
+int main() {
+  SM_TRACE("Starting Celeste...");
+  SM_ASSERT(window::create_window(800, 600, "Celeste Window"), "Failed to create window!");
+
+  SM_TRACE("Loading OpenGL functions...");
+  gl_renderer::load_gl_functions();
+
+  SM_TRACE("Starting game loop...");
+  while(game::running) { window::update_window(); }
+
+  SM_TRACE("Stopping Celeste...");
   return 0;
 }
